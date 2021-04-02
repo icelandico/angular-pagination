5d38c6b9064f24e258baf002ca287a598255c1b0
Michal Muszynski <m.muszy@gmail.com>
2021-04-02 20:56:22 +0200

    Fetch data in costructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface IData {
+  useId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-pagination';
+  displayData: IData[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    const dataConfig$ = this.http.get('https://jsonplaceholder.typicode.com/posts');
+    dataConfig$.subscribe((data: IData[]) => this.displayData = data);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { PaginationComponent } from './pagination/pagination.component';
+import { DataListComponent } from './data-list/data-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaginationComponent
+    PaginationComponent,
+    DataListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
